226a93f2071f61b11ff23e3907bb39d85ae322e7
Nilirias <sepulvedac.nina@gmail.com>
2020-10-20 18:20:22 -0500

    Practica con arreglos, componentes y clases

---
+++ curriculahome/src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="todo in todos"  v-bind:key="todo.id">
+            <TodoItem v-bind:todo="todo" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: 'Todos',
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
